642da612c4cca45cbfc1d12015f8605837d9c2cd
Sujith Kumar R <sujithkumar.sk175@gmail.com>
2024-11-28 10:34:58 +0530

    added cors

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const User = require("./Models/User");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 require("dotenv").config();
